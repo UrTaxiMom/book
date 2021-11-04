1d6a57baa9945f53791f12b93be787864e48b0ea
chenyx <karta15626899@gmail.com>
2021-11-04 16:28:02 +0800

    Initial Commit

---
+++ book/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  book
+//
+//  Created by 陳以晢 on 2021/11/4.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ book/bookApp.swift
@@ -0,0 +1,17 @@
+//
+//  bookApp.swift
+//  book
+//
+//  Created by 陳以晢 on 2021/11/4.
+//
+
+import SwiftUI
+
+@main
+struct bookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
